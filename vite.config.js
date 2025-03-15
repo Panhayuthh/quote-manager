3bde40f989be67691e06909743aa02f5249a5a60
Panhayuth Oeun <panhayuth11@gmail.com>
2025-03-15 13:23:37 +0700

    fix: changing env

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         '/api': {
-          target: env.API_URL,
+          target: env.VITE_API_URL,
           changeOrigin: true,
           secure: true,
           withCredentials: true,
