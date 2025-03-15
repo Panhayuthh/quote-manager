29b8348f1de65a5baaf1555d9f4a8389d897ef0b
Panhayuth Oeun <panhayuth11@gmail.com>
2025-03-15 11:56:46 +0700

    add: env config

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true,
-        headers: {
-          Accept: 'application/json',
-          "Content-Type": 'application/json',
+export default defineConfig(({ mode }) => {
+  // Load environment variables
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.API_URL,
+          changeOrigin: true,
+          secure: true,
+          withCredentials: true,
+          headers: {
+            Accept: 'application/json',
+            "Content-Type": 'application/json',
+          }
         }
       }
     }
-  }
-})
+  };
+});
